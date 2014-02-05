58dc097da818b7803bb70c389ad72543cbc0ec88
Philipp Glatza <pglatza@users.noreply.github.com>
2014-02-05 09:08:34 +0000

    added localdefs.sh

---
+++ adaptions/common/calabash/localdefs.sh
@@ -0,0 +1,7 @@
+case "`hostname`" in
+  don) JAVA=/usr/lib/jvm/java-6-sun/bin/java;;
+  pollux) JAVA=/usr/lib/jvm/java-6-sun/bin/java;;
+  aspect) JAVA=/usr/lib/jvm/java-6-sun/bin/java;;
+  worker0) JAVA=/data/ASSP/ASSP_chroot/usr/local/bin/java16;;
+esac
+
